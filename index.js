decad5aaf7c0ea4bb210d3d1a2262f11833c42cf
kkafi09 <afi.firda@gmail.com>
2023-03-25 10:03:34 +0700

    feat: club

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
 const logger = require("./helpers/utils/logger");
-const config = require("./helpers/infra/global_config")
+const config = require("./helpers/infra/global_config");
 const port = config.get("/port");
 const morgan = require("morgan");
 
 const userApi = require("./api/user");
+const clubApi = require("./api/club");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 app.use("/api/user/v1", userApi);
+app.use("/api/club/v1", clubApi);
 
 app.get("/", (req, res) =>
   res.status(200).json({ message: "Welcome to the API STAPA App" })
@@ -23,5 +25,9 @@ app.get("/", (req, res) =>
 
 app.listen(port, () => {
   const ctx = "app-listen";
-  logger.log(ctx, `This API Service STAPA is running properly and listening on port ${port}`, 'initate application');
+  logger.log(
+    ctx,
+    `This API Service STAPA is running properly and listening on port ${port}`,
+    "initate application"
+  );
 });
